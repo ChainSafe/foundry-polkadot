30717d75822e4add90e6f561395b83904061b39e
giuseppere <giuseppe.re@parity.io>
2025-09-15 18:20:55 +0200

    Add extra fields

--- crates/anvil-polkadot/src/genesis.rs
+++ crates/anvil-polkadot/src/genesis.rs
@@ -9,26 +9,36 @@ use std::collections::BTreeMap;
 /// Genesis settings
 #[derive(Clone, Debug, Default)]
 pub struct GenesisConfig {
+    /// The chain id of the Substrate chain, if provided.
+    pub chain_id: Option<u64>,
     /// The initial timestamp for the genesis block
     pub timestamp: Option<u64>,
     /// The genesis block author address, if provided.
     pub coinbase: Option<Address>,
-    /// Balance for genesis accounts
-    pub balance: U256,
     /// All accounts that should be initialised at genesis with their info.
     pub alloc: Option<BTreeMap<Address, GenesisAccount>>,
     /// The initial number for the genesis block
     pub number: Option<u64>,
+    /// The genesis header base fee
+    pub base_fee_per_gas: Option<u128>,
+    /// The genesis header gas limit.
+    pub gas_limit: Option<u64>,
 }
 
 impl From<AnvilNodeConfig> for GenesisConfig {
     fn from(anvil_config: AnvilNodeConfig) -> Self {
         Self {
+            chain_id: anvil_config.get_chain_id(),
             timestamp: anvil_config.get_genesis_timestamp(),
             coinbase: anvil_config.genesis.as_ref().map(|g| g.coinbase),
-            balance: anvil_config.genesis_balance,
             alloc: anvil_config.genesis.as_ref().map(|g| g.alloc),
             number: anvil_config.get_genesis_number(),
+            base_fee_per_gas: anvil_config.get_base_fee(),
+            gas_limit: if cfg.disable_block_gas_limit {
+                None
+            } else {
+                Some(anvil_config.gas_limit)
+            },
         }
     }
 }
